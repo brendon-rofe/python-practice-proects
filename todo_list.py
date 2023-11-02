1c3ecb49c5bf175fae1c1e8d1da5dd1a5572bfdf
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2023-11-02 19:48:00 +0200

    Initial todo list setup

---
+++ todo_list.py
@@ -0,0 +1,10 @@
+def display_menu():
+  print("\nTo-Do List")
+  print("------------")
+  print("1. View tasks")
+  print("2. Add task")
+  print("3. Remove task")
+  print("4. Exit")
+
+if __name__ == "__main__":
+  display_menu()
