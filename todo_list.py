e5b18d9b1a0aa55581e55d406449662c188da476
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2023-11-03 07:21:42 +0200

    Add initial functionality

--- todo_list.py
+++ todo_list.py
@@ -6,5 +6,29 @@ def display_menu():
   print("3. Remove task")
   print("4. Exit")
 
+  choice = input("Choose an option: ")
+  return choice
+
+tasks = []
+
+def add_task():
+  task = input("Enter the task you want to add: ")
+
+  tasks.append(task)
+  print(f"Task '{task}' added!")
+
 if __name__ == "__main__":
-  display_menu()
+  while True:
+    user_choice = display_menu()
+
+    if user_choice == "1":
+      print(tasks)
+    elif user_choice == "2":
+      add_task()
+    elif user_choice == "3":
+      pass
+    elif user_choice == "4":
+      print("Exiting...")
+      break
+    else:
+      print("Invalid option, please choose a number between 1 and 4.")
