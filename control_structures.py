e66a7139faeb32a6cc575d9b1823ca8f1d62e8c7
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2023-11-05 18:22:34 +0200

    control structures

---
+++ control_structures.py
@@ -0,0 +1,37 @@
+# Practice Problem 1
+print('Practice Problem 1')
+
+number = 0
+
+if number >= 1:
+  print('Number is positive')
+elif number <= -1:
+  print('Number is negative')
+elif number == 0:
+  print('Number is zero')
+
+
+# Practice Problem 2
+print('Practice Problem 2')
+
+for i in range(10):
+  num = i + 1
+  if num %2 == 0:
+    print(num)
+
+# Practice Problem 3
+print('Practice Problem 3')
+
+x = 0
+
+while x < 5:
+  print(x)
+  x += 1
+
+# Practice Problem 4
+print('Practice Problem 4')
+
+favorite_foods = ['Pizza', 'Burger', 'Pasta', 'Tuna']
+
+for i in favorite_foods:
+  print(i)
