d23bea1c661e5fa09aab93cb167cbd768d179b54
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2023-11-05 17:18:24 +0200

    Create greeting.py

---
+++ greeting.py
@@ -0,0 +1,11 @@
+# Ask for the user's name
+user_name = input("What is your name? ")
+
+# Ask for the user's age
+user_age = input("How old are you? ")
+
+# Calculate the age in 5 years
+age_in_5_years = int(user_age) + 5
+
+# Print out a greeting and tell the user how old they will be in 5 years
+print(f'Greeting {user_name}! You will be {age_in_5_years} in 5 years!')
