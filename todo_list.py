a4fedb8dab0b28fd99e5c7c7327778dfa9d29e7c
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2023-11-19 20:11:10 +0200

    Implement update task by index functionality

--- todo_list.py
+++ todo_list.py
@@ -1,10 +1,9 @@
-tasks = []
-
 def main_menu():
   while True:
     print('\n=== Todo List ===')
     print('1. Create task')
     print('2. List all tasks')
+    print('3. Update task by index')
     print('5. Exit')
 
     choice = input('Enter a corrisponding number, to make a choice: ')
@@ -13,6 +12,8 @@ def main_menu():
       create_task()
     elif choice == '2':
       list_tasks()
+    elif choice == '3':
+      update_task_by_index()
     elif choice == '5':
       break
     else:
@@ -24,6 +25,14 @@ def create_task():
 
 def list_tasks():
   print(tasks)
+
+def update_task_by_index():
+  index = int(input('Task at which index?: '))
+  updatedTask = input('Update to apply: ')
+  tasks[index] = updatedTask
+  print(f'task at index {index} updated.')
+
+tasks = []
 
 if __name__ == '__main__':
   main_menu()
