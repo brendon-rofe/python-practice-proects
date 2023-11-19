e3049e7ea1e76febaee5881c1df203156468b07b
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2023-11-19 19:33:25 +0200

    Implement list all tasks

--- todo_list.py
+++ todo_list.py
@@ -4,12 +4,15 @@ def main_menu():
   while True:
     print('\n=== Todo List ===')
     print('1. Create task')
+    print('2. List all tasks')
     print('5. Exit')
 
     choice = input('Enter a corrisponding number, to make a choice: ')
 
     if choice == '1':
       create_task()
+    elif choice == '2':
+      list_tasks()
     elif choice == '5':
       break
     else:
@@ -19,7 +22,8 @@ def create_task():
   task = input('Please type in your task: ')
   tasks.append(task)
 
-print(tasks)
+def list_tasks():
+  print(tasks)
 
 if __name__ == '__main__':
   main_menu()
