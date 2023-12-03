9dffd874ecc351c5dedfec759e91214735c8e7a3
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2023-12-03 14:59:02 +0200

    Get proper data

--- ecom_inv_mgmnt/explore_sales_data.py
+++ ecom_inv_mgmnt/explore_sales_data.py
@@ -5,3 +5,14 @@ file_path = 'ecom_inv_mgmnt\inventory_sales_data.csv'
 data = pd.read_csv(file_path)
 
 print(data.head())
+
+print(data.isnull().sum())
+
+data['Date'] = pd.to_datetime(data['Date'])  # Converting Date column to datetime
+daily_sales = data.groupby('Date')['Quantity_Sold'].sum()
+plt.figure(figsize=(12, 6))
+plt.plot(daily_sales)
+plt.title('Total Sales per Day')
+plt.xlabel('Date')
+plt.ylabel('Quantity Sold')
+plt.show()
